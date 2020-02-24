707c0f589ddd8bff980896092c5f73da7814a5cf
ZephyrBlu <lukejholroyd@gmail.com>
2020-02-25 03:01:26 +1300

    Add recent and missed replay uploads
    Added the ability for users to upload their most recent 100 replays if they haven't uploaded any replays before. Also added a CSP and fixed minor bugs for distribution.

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const { app, BrowserWindow, Tray, Menu } = require('electron');
 const username = require('username');
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
+const fetch = require('node-fetch');
 const login = require('./login');
 const watchReplays = require('./replayWatcher');
 
 global.dir = { path: null };
+global.uploadQueue = { queue: [], inProgress: false };
+global.token = { token: null };
 
 let win;
 function createWindow() {
     win = new BrowserWindow({
         width: 1000,
-        height: 600,
+        height: 800,
         frame: false,
         webPreferences: {
             nodeIntegration: true,
         },
+        resizable: false,
     });
     win.loadFile('index.html');
     win.on('close', (event) => {
@@ -33,13 +40,58 @@ app.on('ready', async () => {
         }
     }]);
 
-    tray = new Tray('favicon.png');
+    tray = new Tray(path.join(__dirname, 'favicon.png'));
     tray.setTitle('Zephyrus Replay Auto-Uploader');
     tray.on('click', () => {
         win.show();
     });
     tray.setToolTip('Zephyrus Replay Auto-Uploader');
     tray.setContextMenu(contextMenu);
+
+    setInterval(async () => {
+        if (!global.uploadQueue.inProgress) {
+            const failedUploads = [];
+            global.uploadQueue.inProgress = true;
+            await Promise.all(global.uploadQueue.queue.map(([replayFilePath, replaySet], index) => {
+                return new Promise((resolve, reject) => {
+                    fs.readFile(replayFilePath, async (err, replayFile) => {
+                        if (err) {
+                            console.error(err);
+                            return
+                        }
+
+                        const replayHash = crypto.createHash('md5').update(replayFile).digest('hex');
+                        if (!replaySet.includes(replayHash)) {
+                            // const url = 'https://zephyrus.gg/api/upload/';
+                            const url = 'https://testing-dot-reflected-codex-228006.appspot.com/api/upload/';
+                            const result = await fetch(url, {
+                                method: 'POST',
+                                headers: {
+                                    Authorization: `Token ${global.token.token}`,
+                                },
+                                body: replayFile,
+                            }).then(response => (
+                                response
+                            )).catch((error) => {
+                                failedUploads.push([replayFilePath, replaySet]);
+                            });
+
+                            if (result) {
+                                if (result.ok) {
+                                    const message = { e: 'updateAll', replayHash: replayHash };
+                                    win.webContents.send('update', JSON.stringify(message));
+                                }
+                            }
+                        } else {
+                        }
+                        resolve();
+                    });
+                });
+            }));
+            global.uploadQueue.inProgress = false;
+            global.uploadQueue.queue = failedUploads;
+        }
+    }, 10000);
 });
 
 app.on('activate', () => {
--- replayWatcher.js
+++ replayWatcher.js
@@ -1,16 +1,15 @@
 const chokidar = require('chokidar');
-const fs = require('fs');
 const username = require('username');
-const fetch = require('node-fetch');
+const { ipcRenderer } = require('electron');
 const remote = require('electron').remote;
 
 let watcher;
 
-async function watchReplays(token) {
+async function watchReplays(replaySet) {
     let accountPath;
     let fullPath;
+    const replayPath = '/**/Replays/Multiplayer/*.SC2Replay';
     if (!remote.getGlobal('dir').path) {
-        const replayPath = '/**/Replays/Multiplayer/*.SC2Replay';
         const user = await username();
 
         if (process.platform === 'win32') {
@@ -22,42 +21,31 @@ async function watchReplays(token) {
         remote.getGlobal('dir').path = accountPath;
         fullPath = accountPath + replayPath;
     } else {
-        fullPath = remote.getGlobal('dir').path;
+        fullPath = remote.getGlobal('dir').path + replayPath;
     }
 
     watcher = chokidar.watch(fullPath, {
         ignoreInitial: true,
     });
 
-    const url = 'https://zephyrus.gg/api/upload/';
     watcher.on('add', async (filePath) => {
-        const result = await fetch(url, {
-            method: 'POST',
-            headers: {
-                Authorization: `Token ${token}`,
-            },
-            body: fs.readFileSync(filePath),
-        }).then(response => response);
-
-        if (result.ok) {
-            console.log('success');
-        } else {
-            console.log('fail');
-        }
+        const replayQueue = remote.getGlobal('uploadQueue').queue;
+        replayQueue.push([filePath, replaySet]);
+        remote.getGlobal('uploadQueue').queue = replayQueue;
     });
 };
 
-async function stopWatchingReplays() {
+async function restart(replaySet) {
     await watcher.close();
+    await watchReplays(replaySet);
 };
 
-async function restart(token) {
-    await stopWatchingReplays();
-    await watchReplays(token);
-};
+async function close() {
+    await watcher.close();
+}
 
 module.exports = {
     watchReplays: watchReplays,
-    stopWatchingReplays: stopWatchingReplays,
     restart: restart,
+    close: close,
 };
